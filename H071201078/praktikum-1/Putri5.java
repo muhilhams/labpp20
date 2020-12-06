c1872af50da547688ff4492d2e69ae006e5a5187
ptrzky <ptrameilya99@gmail.com>
2020-12-06 19:33:44 +0800

    tugas 3

--- H071201078/praktikum-1/Putri5.java
+++ H071201078/praktikum-1/Putri5.java
@@ -13,6 +13,7 @@ public class Putri5 {
         if (j >= 0 && j < 360){
             float d = j*240;
             int x = (int)d;
+
             h=(x/3600);
             h = h+6;
             if (h>23){
@@ -20,27 +21,31 @@ public class Putri5 {
             }
             else {
                 h +=0;
+            }
                 m = (x%3600);
                 m = m/60;
+
                 s = (x%3600);
                 s = s%60;
             System.out.printf("%02d: %02d: %02d \n" ,h,m,s);
-            }
+            
 
-        if (x>0&&x<=14400){
+        if (x> 0&&x <=14400){
             System.out.println("Selamat Pagi");
         }
-        if (x>14400&&x<=32400){
+        if (x> 14400&&x <=32400){
             System.out.println("Selamat Siang");
         }
-        if (x>32400&&x<=46800){
+        if (x > 32400&&x <=46800){
             System.out.println("Selamat Sore");
         }
-        if (x>46800&&x<=86400){
+        if (x > 46800&&x <=86900){
             System.out.println("Selamat Malam");
         
-        }else {
-            System.out.println("Perhatikan besaran inputan derajat");
+        }
+        
+         else {
+           System.out.println("Perhatikan besaran inputan derajat");
 
         }
         }
